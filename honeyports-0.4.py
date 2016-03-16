7cd700b08fbea81d64253323b93004fc9bf8767e
Andy Johnson <andyjohnsonfl@gmail.com>
2016-03-16 13:55:10 -0400

    Added WinEventLogging and python3 compat
    Added the ability to log to Windows Event Log, and made compatible with python3.

--- honeyports-0.4.py
+++ honeyports-0.4.py
@@ -5,6 +5,8 @@
 
 # Date: 7/28/13
 
+# Updated 20160316 Andy Johnson -- Added Windows Event Warning and made python3 compatible
+
 # Description: This script listens on a port. When a full connection is made to that port
 # Honeyports will create a firewall rule blocking the source IP address.
 # Currently works on Windows, Linux and OS X with the built-in firewalls.
@@ -16,10 +18,13 @@
 # Import the stuff we need
 
 import socket               # Import socket module
-import platform		    # Import platform module to determine the os
+import platform            # Import platform module to determine the os
 from subprocess import call # Import module for making OS commands (os.system is deprecated)
-import sys, getopt	    # Import sys and getopt to grab some cmd options like port number
-import os		    # Import os because on Linux to run commands I had to use popen
+import sys, getopt        # Import sys and getopt to grab some cmd options like port number
+import os            # Import os because on Linux to run commands I had to use popen
+# This makes this far less OS agnostic, just deal with it.
+import win32evtlogutil, win32evtlog
+
 
 # Declare some useless variables to display info about this script
 
@@ -29,7 +34,7 @@ name="Honeyports"
 
 # Message to send via TCP to connected addresses
 # Can be used to mimic a banner
-nasty_msg = "\n\n***** Fuck You For Connecting *****\n\n" 
+nasty_msg = b"\n\n***** Thank You For Connecting *****\n\n" 
 
 # Command line options!
 
@@ -47,20 +52,20 @@ for o, a in myopts:
       except:
          print("Not a valid port")
          sys.exit(2)
-		 
+         
    elif o == '':
       print("Usage: %s -p port (Required)" % sys.argv[0])
       sys.exit(2)
    else:
       print("Usage: %s -p port " % sys.argv[0])
  
-print name, 'Version:', version
-print 'I will listen on TCP port number: ', port 
+print("%s Version: %s" % (name, version))
+print("I will listen on TCP port number: %s" % port) 
 
 # Determine which platform we are running on
 
 platform = platform.system()
-print 'Honeyports detected you are running on: ', platform
+print("Honeyports detected you are running on: %s" % platform)
 
 # Start Listener
 
@@ -75,83 +80,85 @@ s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 # Which interface to listen on
 
 if platform == "Darwin":
-	host = socket.gethostname() # Get local IP on OS X
-	print "Setting sockets up for OS X"
+    host = socket.gethostname() # Get local IP on OS X
+    print("Setting sockets up for OS X")
 elif platform == "Linux":
-	host = s.getsockname()[0] # Get local IP on Linux
-	print "Setting sockets up for Linux"
+    host = s.getsockname()[0] # Get local IP on Linux
+    print("Setting sockets up for Linux")
 else:
-	host = ''
-	
+    host = ''
+    
 if port >= 1 and port <= 65535:
    try:
       s.bind((host, port))        # Bind to the port
-   except socket.error, e:
+   except socket.error as e:
       print("Unable to bind to port with error: {0} -- {1} ".format( e[0], e[1]))
       sys.exit(2)
 else:
    print("Usage: %s -p port " % sys.argv[0])
-   print 'Please specify a valid port range (1-65535) using the -p option'
+   print("Please specify a valid port range (1-65535) using the -p option")
    sys.exit(2)
 
 s.listen(5)                 # Now wait for client connection.
-print 'Listening on ', host, ' : ', port
+print("Listening on %s:%s" % (host, port))
 
 while True:
    c, addr = s.accept()     # Establish connection with client.
    hostname = str(addr[0])
-   print 'Got connection from', hostname
+   print("Got connection from %s" % hostname)
    c.send(nasty_msg)
-   print 'Blocking the address: ', hostname
+   print("Blocking the address: %s" % hostname)
 
    # If there is a full connection, create a firewall rule
    # Run the command associated with the platform being run:
 
    if platform == "Windows":
-	print "Creating a Windows Firewall Rule\n"
-        fw_result = call('netsh advfirewall firewall add rule name="honeyports" dir=in remoteip= ' + hostname + ' localport=any protocol=TCP action=block > NUL', shell=True)
-	flush = 'netsh', 'advfirewall reset'
-	fwlist = "netsh", """ advfirewall firewall show rule name=honeyports | find "RemoteIP" """
-	
+       print("Sending alert to EVENT LOG")
+       win32evtlogutil.ReportEvent("HoneyPortAlert", 4242, eventType = win32evtlog.EVENTLOG_WARNING_TYPE, strings = [ "Detected HoneyPort Hit", "HoneyPort hit from " + hostname + " on port " + str(port) ], data = (hostname + "\0" + str(port)).encode("ascii"));
+       print("Creating a Windows Firewall Rule\n")
+       fw_result = call('netsh advfirewall firewall add rule name="honeyports" dir=in remoteip= ' + hostname + ' localport=any protocol=TCP action=block > NUL', shell=True)
+       flush = 'netsh', 'advfirewall reset'
+       fwlist = "netsh", """ advfirewall firewall show rule name=honeyports | find "RemoteIP" """
+    
    elif platform == "Linux":
-	print "Creating a Linux Firewall Rule\n"
-	fw_result = os.popen( '/sbin/iptables -A INPUT -s ' + hostname + ' -j REJECT' )
-	if fw_result:
-		fw_result=0
-	else:
-		fw_result=1
+    print("Creating a Linux Firewall Rule\n")
+    fw_result = os.popen( '/sbin/iptables -A INPUT -s ' + hostname + ' -j REJECT' )
+    if fw_result:
+        fw_result=0
+    else:
+        fw_result=1
 
-	flush = 'iptables', '-F'
-	fwlist = 'iptables', '-nL'
+    flush = 'iptables', '-F'
+    fwlist = 'iptables', '-nL'
 
    elif platform == "Darwin":
-	print "Creating a Mac OS X Firewall Rule\n"
-        fw_result = call(['ipfw', '-q add deny src-ip ' + hostname])
-	flush = 'ipfw', '-q flush'
-	fwlist = 'ipfw', 'list'
+    print("Creating a Mac OS X Firewall Rule\n")
+    fw_result = call(['ipfw', '-q add deny src-ip ' + hostname])
+    flush = 'ipfw', '-q flush'
+    fwlist = 'ipfw', 'list'
 
    if fw_result:
-      print 'Crapper, firewall rule not added'
+      print("Crapper, firewall rule not added")
    else:
-     print 'I just blocked: ', hostname
+     print("I just blocked: %s" % hostname)
 
    c.close()                # Close the connection
 
 # Listen for user to type something then give options to
 # Flush all firewall rules, quit or print the rules
 
-   mydata = raw_input('Enter Commands: q=quit f=flush rules p=print rules.')
+   mydata = input('Enter Commands: q=quit f=flush rules p=print rules.')
 
    if mydata == "f":
-	print "Flushing firewall rules..."
-	print 'Flush command is: ', str(flush)
-	call(flush[0] + ' ' + flush[1], shell=True)
+    print("Flushing firewall rules...")
+    print("Flush command is: %s" % str(flush))
+    call(flush[0] + ' ' + flush[1], shell=True)
    elif mydata == "p":
-	print 'Here is what your rules look like:'
-	call(fwlist[0] + ' ' +  fwlist[1], shell=True)
+    print("Here is what your rules look like:")
+    call(fwlist[0] + ' ' +  fwlist[1], shell=True)
    elif mydata == "q":
-	if platform == "Linux":
-	   s.shutdown(1)
-	s.close()
-	print "Goodbye."
-	exit()
+    if platform == "Linux":
+       s.shutdown(1)
+    s.close()
+    print("Goodbye.")
+    exit()
